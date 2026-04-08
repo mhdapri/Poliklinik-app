d1d350f912c2bd3d053c3d948fe7bbb48bd37f57
mhdapri <apri.yanto1324@gmail.com>
2026-04-08 21:29:49 +0700

    tugas 2

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app title="Admin Dashboard">
+    <h1 class="ml-4">hallo selamat datang admin</h1>
+</x-layouts.app>
---
+++ resources/views/pasien/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app title="Pasien Dashboard">
+    <h1 class="ml-4">hallo selamat datang pasien</h1>
+</x-layouts.app>
